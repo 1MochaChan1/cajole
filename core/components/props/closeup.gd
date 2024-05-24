27d727c7728f9a878bb08195ccfa1e9d1326b422
1MochaChan1 <danny8doo@gmail.com>
2024-05-24 12:22:10 +0530

    passcode screen added

--- core/components/props/closeup.gd
+++ core/components/props/closeup.gd
@@ -1,11 +1,17 @@
 class_name Closeup extends Area2D
 
 @export var camera:Camera2D
+@export var interact_obj:Node
+
+signal on_closeup(_interactable_node)
+signal on_closeup_exit(_interactable_node)
 
 func switch_camera():
 	camera.enabled=true
 	camera.make_current()
+	on_closeup.emit(interact_obj)
 
 func switch_back_camera():
 	camera.enabled = false
 	camera.is_current()
+	on_closeup_exit.emit(interact_obj)
--- levels/village_deeper.gd
+++ levels/village_deeper.gd
@@ -1,8 +1,30 @@
 extends CustomScene
 
+@export var dialogue_caller:Actionable
+
+var passcode:int = 191620
+
 func _ready():
 	initialize()
 	var enemies = get_tree().get_nodes_in_group("Enemies")
 	for enemy in enemies:
 		if(enemy is Enemy):
 			enemy.player = player
+
+# Show Password Tab
+func _on_closeup_lock_on_closeup(_interactable_node:Node):
+	_interactable_node.show()
+
+
+func _on_line_edit_text_changed(new_text):
+	if(len(new_text)==6):
+		if(new_text==str(passcode)):
+			dialogue_caller.dialogue_start='right_passcode'
+			# unlock door #
+		else:
+			dialogue_caller.dialogue_start='wrong_passcode'
+		dialogue_caller.action()
+
+
+func _on_closeup_lock_on_closeup_exit(_interactable_node):
+	_interactable_node.hide()
